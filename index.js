a287d412f7a64127ba74060275df91832950770e
Max Tarver <maxotar@users.noreply.github.com>
2021-01-21 11:28:50 -0800

    basic signup signin signout

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const usersRepo = require("./repositories/users");
+const cookieSession = require("cookie-session");
+const authRouter = require("./routes/admin/auth");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email });
-
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-
-  res.send("Account Created");
-});
+app.use(
+  cookieSession({
+    keys: ["secret"],
+  })
+);
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
